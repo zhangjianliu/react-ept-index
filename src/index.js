3e9edea27bd156cbd9e5fbe3b1f2e19394d1a8cc
zhangjianliu <3190547371@qq.com>
2019-01-18 17:39:22 +0800

    react 基本配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /*使用es6的箭头函数*/
-var func = str => {
-    document.getElementById('app').innerHTML = str;
-};
-func('我现在在使用Babel!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Hello from './conpoments/Hello/hello'
+ReactDom.render(<Hello/>, document.getElementById('app'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-
+    // 模式
+    mode: "development",
     /*入口*/
     entry: path.join(__dirname, './src/index.js'),
 
@@ -23,5 +24,15 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    // 开发服务器
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),// 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
+        historyApiFallback: true,// 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        compress: true,// 启用gzip压缩
+        inline: true,// 设置为true，当源文件改变时会自动刷新页面
+        hot: false,// 模块热更新，取决于HotModuleReplacementPlugin
+        host: '127.0.0.1',// 设置默认监听域名，如果省略，默认为“localhost”
+        port: 3001// 设置默认监听端口，如果省略，默认为“8080”
+    },
 };
